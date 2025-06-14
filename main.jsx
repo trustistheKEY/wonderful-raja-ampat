aee136bd308e5dc7628d95f585dddf600f79e3cc
trustistheKEY <go2rajaampat@gmail.com>
2025-06-14 23:59:31 +0900

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import WonderfulRajaAmpat from "./WonderfulRajaAmpat";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <WonderfulRajaAmpat />
+  </BrowserRouter>
+);
